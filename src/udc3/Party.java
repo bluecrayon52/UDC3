e1a6bfb5dff5b5cdaf481941a66aac068ab03f68
nathanielclayarnold <nathanielclayarnold@192.168.0.9>
2017-04-25 15:28:36 -0400

    Progress on Enum Types

---
+++ src/udc3/Party.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package udc3;
+
+/**
+ *
+ * @author nathanielclayarnold
+ */
+public enum Party {
+    REPUBLICAN,
+    DEMCRATIC,
+    INDEPENDANT,
+    OTHER; 
+}
---
+++ src/udc3/State.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package udc3;
+
+/**
+ *
+ * @author nathanielclayarnold
+ */
+public enum State {
+    AL, AK, AZ, AR, 
+    CA, CO, CT, 
+    DE, 
+    FL, 
+    GA, 
+    HI, 
+    ID, IL, IN, IA,
+    KS, KY,
+    LA,
+    ME, MD, MA, MI, MN, MS, MO, MT, 
+    NE, NV, NH, NJ, NM, NY, NC, ND, 
+    OH, OK, OR, PA, RI, 
+    SC, SD, 
+    TN, TX, 
+    UT, 
+    VT, VA, 
+    WA, WV, WI, WY;
+    
+}
